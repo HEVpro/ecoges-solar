b1ad3fc18344756faef43b3ea835fcdbffe38dec
Hector <hector.evalverde@gmail.com>
2022-05-08 19:32:21 +0200

    feat: hero responsive

--- components/hero.tsx
+++ components/hero.tsx
@@ -6,12 +6,12 @@ const Hero = () => {
     return (
         <div className="relative">
             <div
-                className="absolute z-30 p-5 text-2xl text-white bg-opacity-60 bg-gray-800 w-full h-full rounded-bl-[85px]"
+                className="absolute z-30 text-2xl text-white bg-opacity-60 bg-gray-800 w-full h-full rounded-bl-[85px]"
             >
-                <div className="flex flex-col pt-24 w-full text-center items-center">
-                    <h1 className="text-white text-4xl sm:text-6xl font-semibold w-[20ch] leading-tight font-Inter">{Data.hero.title}</h1>
-                    <h1 className="text-white text-md  w-[36ch] sm:w-[50ch] my-6">{Data.hero.description}</h1>
-                    <PrimaryButton target="#" content={Data.hero.button} classname="w-36 mb-8"/>
+                <div className="flex flex-col pt-10 px-8 w-full text-center items-center sm:pt-4 lg:pt-10">
+                    <h1 className="text-white text-4xl sm:text-6xl font-semibold w-[16ch] leading-tight font-Inter mt-10 sm:mt-0 lg:mt-10">{Data.hero.title}</h1>
+                    <h1 className="text-white text-md  w-[30ch] sm:w-[50ch] my-6">{Data.hero.description}</h1>
+                    <button className="w-full mx-8 h-14 border-2 border-white rounded-[25px] font-bold mt-10 sm:w-72 ">{Data.hero.button}</button>
                 </div>
 
             </div>
