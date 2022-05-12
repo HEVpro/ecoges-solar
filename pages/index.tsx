ac805c87858907e442e6c159948e3496a033cec7
Hector <hector.evalverde@gmail.com>
2022-05-12 23:04:41 +0200

    feat: crisp chat

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,17 +32,17 @@ const Home: NextPage = () => {
                 strategy="afterInteractive"
                 dangerouslySetInnerHTML={{
                     __html: `
-                          window.$crisp=[];
-                          window.CRISP_WEBSITE_ID=${process.env.NEXT_PUBLIC_CRISP_ID};
-                          (function(){
-                            const d = document;
-                            const s = d.createElement("script");
-                            s.src = "https://client.crisp.chat/l.js";
-                            s.async = 1;
-                            d.getElementsByTagName("head")[0].appendChild(s);
-                          })();`,
+                      window.$crisp=[];
+                      window.CRISP_WEBSITE_ID="${process.env.NEXT_PUBLIC_CRISP_ID}";
+                      (function(){
+                        const d = document;
+                        const s = d.createElement("script");
+                        s.src = "https://client.crisp.chat/l.js";
+                        s.async = 1;
+                        d.getElementsByTagName("head")[0].appendChild(s);
+                      })();`,
                 }}
-            />;
+            />
             <Navbar/>
             <Hero/>
             <Header/>
