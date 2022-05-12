42cdccda89fee0d760ae08849faf1d3cd04c5732
Hector <hector.evalverde@gmail.com>
2022-05-12 22:00:59 +0200

    refactor: mission refactor style

--- components/mission.tsx
+++ components/mission.tsx
@@ -22,17 +22,17 @@ const Mission = () => {
             id="mission"
             className="bg-secondaryGreen">
             <div className="max-w-4xl mx-auto px-4 py-16 px-8 sm:pt-20 sm:pb-24 lg:max-w-7xl lg:pt-24 lg:px-8">
-                <h2 className="text-5xl font-extrabold tracking-tight font-Inter">{t('mission')}</h2>
+                <h2 className="text-5xl font-extrabold tracking-tight font-Inter text-center sm:text-left">{t('mission')}</h2>
                 <div
                     className="mt-12 grid grid-cols-1 gap-x-6 gap-y-12 sm:grid-cols-2 lg:mt-16 lg:grid-cols-3 lg:gap-x-8 lg:gap-y-16">
                     {myCards.map((feature, index) => (
                         <div key={index}>
-                            <div className="">
+                            <div className="flex flex-col items-center justify-center sm:items-start">
                                 <span className="flex items-center justify-center h-12 w-12 rounded-md bg-white bg-opacity-10">
                                  <FontAwesomeIcon className="bg-darkGreen text-white w-14 h-14 p-3 rounded-md" icon={icons[feature.icon]}/>
                                 </span>
                             </div>
-                            <div className="mt-6">
+                            <div className="mt-6 text-center sm:text-left">
                                 <h3 className="text-2xl font-medium">{feature.title}</h3>
                                 <p className="mt-2 text-2xl text-stone-400">{feature.description}</p>
                             </div>
