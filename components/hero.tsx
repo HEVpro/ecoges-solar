c61aef3bb7af7e922561a4567168ba701a42b4ad
Hector <hector.evalverde@gmail.com>
2022-05-12 19:15:01 +0200

    bug: resize video mobile

--- components/hero.tsx
+++ components/hero.tsx
@@ -9,13 +9,12 @@ const Hero = () => {
             <div
                 className="absolute z-30 text-2xl text-white bg-opacity-40 bg-gray-800 w-full h-full rounded-bl-[85px]"
             >
-                <div className="flex flex-col pt-10 px-8 w-full text-center items-center sm:pt-4 lg:pt-10">
-                    <h1 className="text-white text-4xl sm:text-6xl font-semibold w-[16ch] leading-tight font-Inter mt-10 sm:mt-20 lg:mt-10">{t('hero-title')}</h1>
-                    <button className="w-full mx-8 h-14 border-2 border-white rounded-[25px] font-bold mt-10 sm:w-72 ">{t('hero-button')}</button>
+                <div className="flex flex-col pt-2 px-12 w-full text-center items-center sm:pt-4 lg:pt-10">
+                    <h1 className="text-white text-4xl sm:text-6xl font-semibold w-[12ch] leading-tight font-Inter  sm:mt-20 lg:mt-10">{t('hero-title')}</h1>
+                    <button className="w-44 mx-8 h-12 border-2 border-white rounded-[25px] font-bold mt-2 sm:mt-10 sm:w-72 ">{t('hero-button')}</button>
                 </div>
 
             </div>
-            {/*TODO: CUT VIDEO WITHOUT DESCRIPTION*/}
             <div className="w-screen sm:hidden">
                 <video autoPlay loop muted playsInline  className="rounded-bl-[85px]">
                     <source src='/video-mobile.mp4' type='video/mp4' media="all"/>
