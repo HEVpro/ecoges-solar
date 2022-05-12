9d428001804732533a0b2a1f60329ccdaf846315
Hector <hector.evalverde@gmail.com>
2022-05-12 16:43:49 +0200

    feat: navbar responsive

--- components/atoms/languages.tsx
+++ components/atoms/languages.tsx
@@ -33,66 +33,68 @@ export default function Languages() {
     }, [locale])
 
     return (
-        <Listbox value={selected} onChange={handleSubmit}>
-            {({open}) => (
-                <>
-                    <Listbox.Label className="sr-only">Change published status</Listbox.Label>
-                    <div className="relative">
-                        <div className="inline-flex shadow-sm rounded-md divide-x divide-green-600">
-                            <div className="relative z-0 inline-flex shadow-sm rounded-md divide-x divide-softGreen">
+            <Listbox value={selected} onChange={handleSubmit}>
+                {({open}) => (
+                    <>
+                        <Listbox.Label className="sr-only">Change published status</Listbox.Label>
+                        <div className="relative mr-2">
+                            <div className="inline-flex shadow-sm rounded-md divide-x divide-green-600 h-10">
                                 <div
-                                    className="relative inline-flex items-center bg-softGreen py-2 pl-3 pr-4 border border-transparent rounded-l-md shadow-sm text-white">
-                                    <CheckIcon className="h-5 w-5" aria-hidden="true"/>
-                                    <p className="ml-2.5 text-sm font-medium">{selected.label}</p>
+                                    className="relative z-0 inline-flex shadow-sm rounded-md divide-x divide-softGreen">
+                                    <div
+                                        className="relative inline-flex items-center bg-softGreen py-2 pl-3 pr-4 border border-transparent rounded-l-md shadow-sm text-white">
+                                        <CheckIcon className="h-5 w-5" aria-hidden="true"/>
+                                        <p className="ml-2.5 text-sm font-medium">{selected.label}</p>
+                                    </div>
+                                    <Listbox.Button
+                                        className="relative inline-flex items-center bg-softGreen p-2 rounded-l-none rounded-r-md text-sm font-medium text-white hover:bg-softGreen focus:outline-none focus:z-10 focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-green-500">
+                                        <span className="sr-only">Change published status</span>
+                                        <ChevronDownIcon className="h-5 w-5 text-white" aria-hidden="true"/>
+                                    </Listbox.Button>
                                 </div>
-                                <Listbox.Button
-                                    className="relative inline-flex items-center bg-softGreen p-2 rounded-l-none rounded-r-md text-sm font-medium text-white hover:bg-softGreen focus:outline-none focus:z-10 focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-green-500">
-                                    <span className="sr-only">Change published status</span>
-                                    <ChevronDownIcon className="h-5 w-5 text-white" aria-hidden="true"/>
-                                </Listbox.Button>
                             </div>
-                        </div>
 
-                        <Transition
-                            show={open}
-                            as={Fragment}
-                            leave="transition ease-in duration-100"
-                            leaveFrom="opacity-100"
-                            leaveTo="opacity-0"
-                        >
-                            <Listbox.Options
-                                className="origin-top-right absolute z-10 right-0 mt-2 w-72 rounded-md shadow-lg overflow-hidden bg-white divide-y divide-gray-200 ring-1 ring-black ring-opacity-5 focus:outline-none">
-                                {publishingOptions.map((option) => (
-                                    <Listbox.Option
-                                        key={option.label}
-                                        className={({active}) =>
-                                            classNames(
-                                                active ? 'text-white bg-softGreen' : 'text-gray-900',
-                                                'cursor-default select-none relative p-4 text-sm'
-                                            )
-                                        }
-                                        value={option}
-                                        onChange={handleSubmit}
-                                    >
-                                        {({selected, active}) => (
-                                            <div className="flex flex-col">
-                                                <div className="flex justify-between">
-                                                    <p className={selected ? 'font-semibold' : 'font-normal'}>{option.label}</p>
-                                                    {selected ? (
-                                                        <span className={active ? 'text-white' : 'text-green-500'}>
+                            <Transition
+                                show={open}
+                                as={Fragment}
+                                leave="transition ease-in duration-100"
+                                leaveFrom="opacity-100"
+                                leaveTo="opacity-0"
+                            >
+                                <Listbox.Options
+                                    className="origin-top-right absolute z-10 right-0 mt-2 w-72 rounded-md shadow-lg overflow-hidden bg-white divide-y divide-gray-200 ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                    {publishingOptions.map((option) => (
+                                        <Listbox.Option
+                                            key={option.label}
+                                            className={({active}) =>
+                                                classNames(
+                                                    active ? 'text-white bg-softGreen' : 'text-gray-900',
+                                                    'cursor-default select-none relative p-4 text-sm'
+                                                )
+                                            }
+                                            value={option}
+                                            onChange={handleSubmit}
+                                        >
+                                            {({selected, active}) => (
+                                                <div className="flex flex-col">
+                                                    <div className="flex justify-between">
+                                                        <p className={selected ? 'font-semibold' : 'font-normal'}>{option.label}</p>
+                                                        {selected ? (
+                                                            <span className={active ? 'text-white' : 'text-green-500'}>
                                                           <CheckIcon className="h-5 w-5" aria-hidden="true"/>
                                                         </span>
-                                                    ) : null}
+                                                        ) : null}
+                                                    </div>
                                                 </div>
-                                            </div>
-                                        )}
-                                    </Listbox.Option>
-                                ))}
-                            </Listbox.Options>
-                        </Transition>
-                    </div>
-                </>
-            )}
-        </Listbox>
+                                            )}
+                                        </Listbox.Option>
+                                    ))}
+                                </Listbox.Options>
+                            </Transition>
+                        </div>
+                    </>
+                )}
+            </Listbox>
+
     )
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import Link from "next/link";
 import PrimaryButton from "./atoms/button";
 import ResponsiveMenu from "./atoms/responsive-menu";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import Languages from "./atoms/languages";
 import {useRouter} from "next/router";
 import {useTranslation} from "next-i18next";
@@ -16,26 +16,28 @@ const icons = require("@fortawesome/free-solid-svg-icons")
 const myIcons = [
     {name: "test", icon: "faBars"}
 ]
-interface Navbar{
+
+interface Navbar {
     name: string;
     link: string;
 }
 
 const Navbar = () => {
     const [menuResponsive, setMenuResponsive] = useState<boolean>(false)
-    const handleResponsiveMenu = () =>{
+    const handleResponsiveMenu = () => {
         menuResponsive ? setMenuResponsive(false) : setMenuResponsive(true)
     }
     const [navbar, setNavbar] = useState<Navbar[]>([])
     const router = useRouter()
-    const { t } = useTranslation('common');
+    const {t} = useTranslation('common');
 
     useEffect(() => {
-        setNavbar(t('navbar-footer', {returnObjects:true}))
-    },[router.locale])
+        setNavbar(t('navbar-footer', {returnObjects: true}))
+    }, [router.locale])
 
     return (
-        <nav className="z-50 w-full bg-darkGreen flex items-center justify-evenly sm:justify-between py-2 px-6 font-Barlow relative text-white">
+        <nav
+            className="z-50 w-full bg-darkGreen flex items-center justify-evenly sm:justify-between py-2  font-Barlow relative text-white">
             <div className="ml-0 mr-4 md:ml-28">
                 <Link href="/">
                     <a>
@@ -46,18 +48,20 @@ const Navbar = () => {
             <div className="hidden lg:block w-1/3  flex justify-between text-white">
                 {navbar.map((x, index) => {
                     return (
-                        <Link key={index} href={"#"+x.link}>
-                            <a className="mx-6 hover:text-white hover:underline hover:decoration-2 hover:decoration-white hover:underline-offset-4">{x.name}</a>
+                        <Link key={index} href={"#" + x.link}>
+                            <a className="mx-3 lg:mx-3 xl:mx-6 hover:text-white hover:underline hover:decoration-2 hover:decoration-white hover:underline-offset-4">{x.name}</a>
                         </Link>
                     )
                 })}
             </div>
-            <div>
-                <Languages />
+            <div className="flex p-1 items-center justify-between">
+                <Languages/>
+                <PrimaryButton target="#Contacto" content="Contacto" classname="mr-0 md:mr-8 hidden sm:block"/>
             </div>
-            <ResponsiveMenu edit={menuResponsive} setEdit={setMenuResponsive} />
-            <button className="block lg:hidden" onClick={handleResponsiveMenu}><FontAwesomeIcon className="text-5xl text-white w-10 h-10" icon={icons[myIcons[0].icon]} /></button>
-            <PrimaryButton target="#Contacto" content="Contacto" classname="mr-0 md:mr-8 hidden sm:block"/>
+            <ResponsiveMenu edit={menuResponsive} setEdit={setMenuResponsive}/>
+            <button className="block lg:hidden" onClick={handleResponsiveMenu}><FontAwesomeIcon
+                className="text-5xl text-white w-10 h-10" icon={icons[myIcons[0].icon]}/></button>
+
         </nav>
     )
 }
