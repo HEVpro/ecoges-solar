ad993cfd03221213a025c7374641d50fb56ce94a
Hector <hector.evalverde@gmail.com>
2022-05-22 11:48:59 +0200

    feat: hero responsive

--- components/hero.tsx
+++ components/hero.tsx
@@ -10,9 +10,9 @@ const Hero = () => {
                 className="absolute top-0 left-0 z-30 text-white w-full h-full bg-opacity-40 bg-gray-800 rounded-bl-[85px]"
             >
                 <div className="flex flex-col px-12 w-full text-center items-center sm:mt-10 sm:pt-4 lg:pt-10">
-                    <h1 className="text-white text-4xl sm:text-6xl font-semibold w-[12ch] leading-tight font-Inter py-2 lg:mt-10">{t('hero-title')}</h1>
+                    <h1 className="text-white text-3xl sm:text-6xl font-semibold w-[16ch] leading-tight font-Inter py-2 mt-4 lg:mt-10">{t('hero-title')}</h1>
                     <button
-                        className="w-44 mx-8 h-12 border-2 border-white rounded-[25px] font-bold mb-6 sm:text-2xl sm:mb-10 mt-2 sm:mt-10 sm:w-72 ">{t('hero-button')}</button>
+                        className="w-44 mx-8 h-12 border-2 border-white rounded-[25px] font-bold mb-10 sm:text-2xl sm:mb-10 mt-2 sm:mt-10 sm:w-72 ">{t('hero-button')}</button>
                 </div>
             </div>
             <div className="w-screen sm:hidden">
