e38d34212190e4ab6c54682a45255539baa67cbc
hector <hector.evalverde@gmail.com>
2022-12-11 20:14:26 +0100

    fb pixel

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,7 @@ export default function Document() {
             <Head>
                 <link rel="preconnect" href="https://fonts.googleapis.com"/>
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
+                <meta name="facebook-domain-verification" content="tf3mm983eln3r5jcivdikzdxpde4if" />
                 <link
                     href="https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,300;0,500;1,200&family=Inter:wght@100;300;400;500&display=swap"
                     rel="stylesheet"/>
